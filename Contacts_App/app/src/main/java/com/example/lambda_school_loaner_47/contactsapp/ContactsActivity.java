79abbc5d999a95d0e7a61a4598fce7691a24cc76
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-17 07:15:40 -0600

    check build version

--- Contacts_App/app/src/main/java/com/example/lambda_school_loaner_47/contactsapp/ContactsActivity.java
+++ Contacts_App/app/src/main/java/com/example/lambda_school_loaner_47/contactsapp/ContactsActivity.java
@@ -2,11 +2,15 @@ package com.example.lambda_school_loaner_47.contactsapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Explode;
+import android.transition.Fade;
 import android.view.View;
+import android.view.Window;
 
 import java.util.ArrayList;
 
@@ -21,6 +25,11 @@ public class ContactsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+            getWindow().setEnterTransition(new Explode());
+            getWindow().setExitTransition(new Fade());
+        }
         setContentView(R.layout.activity_contacts);
 
         context = this;
