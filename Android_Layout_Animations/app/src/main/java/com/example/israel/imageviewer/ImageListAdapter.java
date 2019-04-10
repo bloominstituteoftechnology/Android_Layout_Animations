cff1719d9533f58ece6640ed4e33b153b2c7d7d2
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-10 14:10:43 -0500

    first

---
+++ Android_Layout_Animations/app/src/main/java/com/example/israel/imageviewer/ImageListAdapter.java
@@ -0,0 +1,75 @@
+package com.example.israel.imageviewer;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ViewHolder>  {
+
+    public ImageListAdapter(Context context, ArrayList<ImageData> imageDataArrayList) {
+        this.context = context;
+        this.imageDataArrayList = imageDataArrayList;
+    }
+
+    private Context context;
+    private ArrayList<ImageData> imageDataArrayList;
+
+    @NonNull
+    @Override
+    public ImageListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.image_item_layout, parent, false);
+
+        return new ImageListAdapter.ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ImageListAdapter.ViewHolder holder, int position) {
+
+        final ImageData imageData = imageDataArrayList.get(position);
+        holder.nameTextView.setText(imageData.getName());
+        holder.nameTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open details activity
+                Intent intent = new Intent(context, ImageDetailsActivity.class);
+                intent.putExtra("image data", imageData);
+
+                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation((Activity)context, holder.imageImageView, "transition_image");
+                context.startActivity(intent, activityOptions.toBundle());
+            }
+        });
+
+        holder.imageImageView.setImageURI(imageData.getUri());
+
+        holder.itemView.setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), android.R.anim.slide_in_left));
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageDataArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameTextView = itemView.findViewById(R.id.text_view_name);
+            imageImageView = itemView.findViewById(R.id.image_view_image);
+        }
+
+        private TextView nameTextView;
+        private ImageView imageImageView;
+    }
+}
