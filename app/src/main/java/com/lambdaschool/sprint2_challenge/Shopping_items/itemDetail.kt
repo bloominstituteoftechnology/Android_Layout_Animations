fcf1ed722fe8ad38a578330832ce958719cf7ebb
jDanielDetes <jdanieldetes@gmail.com>
2019-08-19 18:51:27 -0400

    updated animation

--- app/src/main/java/com/lambdaschool/sprint2_challenge/Shopping_items/itemDetail.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/Shopping_items/itemDetail.kt
@@ -1,8 +1,11 @@
 package com.lambdaschool.sprint2_challenge.Shopping_items
 
+import android.app.SharedElementCallback
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.transition.Explode
+import android.view.Window
 import com.lambdaschool.sprint2_challenge.Model.Product
 import com.lambdaschool.sprint2_challenge.R
 import kotlinx.android.synthetic.main.activity_item_detail.*
@@ -15,6 +18,11 @@ class itemDetail : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+        window.enterTransition = Explode()
+        window.exitTransition = Explode()
+
         setContentView(R.layout.activity_item_detail)
 
        val item = intent.getSerializableExtra(ITEM_KEY) as Product
